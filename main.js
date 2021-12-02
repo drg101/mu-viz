b9672a6d14e00411ad56db3dda45f370c831fb13
Daniel Reynolds <danielreynolds101@gmail.com>
2021-12-01 21:15:31 -0700

    pg

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 import './style.css'
 
 const music = new Audio('sound.wav');
-const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-const analyser = audioCtx.createAnalyser();
-setTimeout(() => {
-    music.play();
-    const stream = music.captureStream()
-    const source = audioCtx.createMediaStreamSource(stream);
-    source.connect(analyser);
-    analyser.connect(audioCtx.destination)
-}, 1000)
+
+music.addEventListener("canplay", event => {
+    console.log("here")
+    document.getElementById("play").onclick = () => {
+        music.play()
+        const stream = music.captureStream()
+        const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+        const analyser = audioCtx.createAnalyser();
+        const source = audioCtx.createMediaStreamSource(stream);
+        console.log(source)
+        source.connect(analyser);
+        analyser.connect(audioCtx.destination)
+        analyser.fftSize = 2048;
+        var bufferLength = analyser.frequencyBinCount;
+        console.log(stream.getTracks())
+        setInterval(() => {
+            const dataArray = new Uint8Array(bufferLength);
+            analyser.getByteTimeDomainData(dataArray);
+            
+        }, 100)
+    }
+    
+});
+
 
