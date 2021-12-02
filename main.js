8f290b7887d49761d4429e347924a9872972f03d
Daniel Reynolds <danielreynolds101@gmail.com>
2021-12-01 21:23:47 -0700

    good

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ import './style.css'
 
 const music = new Audio('sound.wav');
 
+let bufferLength = 1024;
+let dataArray = new Uint8Array(bufferLength);
+
 music.addEventListener("canplay", event => {
     console.log("here")
     document.getElementById("play").onclick = () => {
@@ -14,15 +17,47 @@ music.addEventListener("canplay", event => {
         source.connect(analyser);
         analyser.connect(audioCtx.destination)
         analyser.fftSize = 2048;
-        var bufferLength = analyser.frequencyBinCount;
+        bufferLength = analyser.frequencyBinCount;
         console.log(stream.getTracks())
         setInterval(() => {
-            const dataArray = new Uint8Array(bufferLength);
+            dataArray = new Uint8Array(bufferLength);
             analyser.getByteTimeDomainData(dataArray);
-            
+
         }, 100)
     }
-    
+
 });
+
+const c = document.getElementById("myCanvas");
+const ctx = c.getContext("2d");
+const WIDTH = ctx.canvas.width;
+const HEIGHT = ctx.canvas.height;
+
+const draw = () => {
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = 'rgb(0, 0, 0)';
+    ctx.beginPath();
+    var sliceWidth = WIDTH * 1.0 / bufferLength;
+    var x = 0;
+    for (var i = 0; i < bufferLength; i++) {
+
+        var v = dataArray[i] / 128.0;
+        var y = v * HEIGHT / 2;
+
+        if (i === 0) {
+            ctx.moveTo(x, y);
+        } else {
+            ctx.lineTo(x, y);
+        }
+
+        x += sliceWidth;
+    }
+    ctx.lineTo(WIDTH, HEIGHT/2);
+    ctx.stroke();
+
+    requestAnimationFrame(draw)
+}
+requestAnimationFrame(draw)
 
 
